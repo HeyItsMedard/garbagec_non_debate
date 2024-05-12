b0378766f409ff49ad54c2ed3368c474cf5ef02b
gaalgd <117603577+gaalgd@users.noreply.github.com>
2024-05-12 15:03:14 +0200

    id fix

--- Gergő/connection.py
+++ Gergő/connection.py
@@ -10,18 +10,20 @@ with open("data.json", "r") as f:
 
 users_list = []
 for i in range(10000):
-    user = {"name": data["names"][i], "email": data["emails"][i], "age": data["ages"][i]}
+    user = {"name": data["names"][i], "email": data["emails"][i], "age": data["ages"][i], "id": data["ids"][i]}
     users_list.append(user)
 
 def write_to_neo4j(users_list):
     times = []
-    database_connection = GraphDatabase.driver(uri="bolt://localhost:7687", auth=("neo4j", "comparegraphs"))
+    database_connection = GraphDatabase.driver(uri="bolt://localhost:7687", auth=("neo4j", "comparegraph"))
     session = database_connection.session()
     query = """
     UNWIND $users as user
     CREATE (u:User {name: user.name})
     CREATE (e:Email {address: user.email})
     CREATE (a:Age {value: user.age})
+    CREATE (i:Id {value: user.id})
+    CREATE (u)-[:ID]->(i)
     CREATE (u)-[:EMAIL]->(e)
     CREATE (u)-[:AGE]->(a)
     """
@@ -53,12 +55,12 @@ def write_to_neo4j(users_list):
     return time_list, delete_time
 
 def do_query():
-    database_connection = GraphDatabase.driver(uri="bolt://localhost:7687", auth=("neo4j", "comparegraphs"))
+    database_connection = GraphDatabase.driver(uri="bolt://localhost:7687", auth=("neo4j", "comparegraph"))
     session = database_connection.session()
     times = []
     query = """
-    MATCH (u:User)-[:EMAIL]->(e:Email), (u)-[:AGE]->(a:Age)
-    RETURN u.name AS Name, e.address AS Email, a.value AS Age
+    MATCH (u:User)-[:EMAIL]->(e:Email), (u)-[:AGE]->(a:Age), (u)-[:ID]->(i:Id)
+    RETURN u.name AS Name, e.address AS Email, a.value AS Age, i.value AS Id
     """
     time_list = []
     for _ in range(10):
@@ -72,11 +74,11 @@ def do_query():
 
     data = []
     for record in result:
-        data.append((record["Name"], record["Email"], record["Age"]))
+        data.append((record["Name"], record["Email"], record["Age"], record["Id"]))
     
     session.close()
     
-    df = pd.DataFrame(data, columns=["Name", "Email", "Age"])
+    df = pd.DataFrame(data, columns=["Name", "Email", "Age", "Id"])
     return df, time_list
 
 def plots(time_list_write, time_list_delete, time_list_query):
