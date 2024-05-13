0dc86aa0ece9a5282a1a6cae7755b3f377ba269f
Skriba-Izabella <czvlwt@uni-sopron.hu>
2024-05-13 18:20:51 +0200

    uj verzio

--- Iza/key_value.py
+++ Iza/key_value.py
@@ -5,44 +5,38 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 with open("data.json", "r") as f:
     data = json.load(f)
 
-
 users_list = []
+user_dict={}
+user_dict = {}
 for i in range(10000):
-    user = {"name": data["names"][i], "email": data["emails"][i], "age": data["ages"][i]}
-    users_list.append(user)
-
+    user_dict[str(data["ids"][i])] = {"_id": data["ids"][i], "name": data["names"][i], "email": data["emails"][i], "age": data["ages"][i]}
 
 uri = "mongodb+srv://skribaizabella:qByd1uBlOIrcvXIC@probacluster.ukmitfl.mongodb.net/?retryWrites=true&w=majority"
-database_name = "User_database_"
-collection_name ="User_collection"
+database_name = "uj_adatbázis_proba"
+collection_name ="uj_collection_proba"
 
 client = MongoClient(uri)
-
 
 db = client[database_name]
 collection = db[collection_name]
 
-def write_to_mongodb(users_list):
+def write_to_mongodb(users_dict):
     times = []
     delete_times = []
     for _ in range(10):
-       
         delete_start = time.time()
         db[collection_name].delete_many({})
         delete_end = time.time()
         delete_times.append(delete_end - delete_start)
         
-       
         start = time.time()
-        db[collection_name].insert_many(users_list)
+        db[collection_name].insert_one(users_dict)
         end = time.time()
         times.append(end - start)
     
-
     avg_write_time = np.mean(times)
     max_write_time = max(times)
     min_write_time = min(times)
@@ -57,17 +51,14 @@ def do_query():
     times = []
     for _ in range(10):
         start = time.time()
-       
         result = list(collection.find({}, {"_id": 0, "name": 1, "email": 1, "age": 1}))
         end = time.time()
         times.append(end - start)
-
 
     avg_query_time = np.mean(times)
     max_query_time = max(times)
     min_query_time = min(times)
     
-
     df = pd.DataFrame(result, columns=["Name", "Email", "Age"])
     
     return df, [avg_query_time, max_query_time, min_query_time]
@@ -97,10 +88,7 @@ def plots(time_list_write, time_list_delete, time_list_query):
     plt.show()
 
 
-time_list_write, time_list_delete = write_to_mongodb(users_list)
-
+time_list_write, time_list_delete = write_to_mongodb(user_dict)
 
 result_df, time_list_query = do_query()
-
-
 plots(time_list_write, time_list_delete, time_list_query)
